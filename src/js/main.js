acece3ebd6add2f851f82e4f60c230c74bd55d30
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-04-16 19:41:38 -0500

    movieapi

--- src/js/main.js
+++ src/js/main.js
@@ -67,7 +67,8 @@ function createCategories(categories, container) {
 
     const categoryTitle = document.createElement("h3");
     categoryTitle.classList.add("category-title");
-    categoryTitle.setAttribute("id", "id" + category.id);
+    // categoryTitle.setAttribute("id", "id" + category.id);
+    categoryTitle.innerHTML = "֍";
     categoryTitle.addEventListener("click", () => {
       location.hash = `#category=${category.id}-${category.name}`;
     });
